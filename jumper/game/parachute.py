909e7a28765e7dc7a71f5ce695afe3e432572acb
Kano Bailey <kanobailey@gmail.com>
2022-10-21 23:32:58 +0900

    final version

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,52 +1,103 @@
 class Parachute:
+    """A parachute and man drawing for the game. 
+    The responsibility of Parachute is to handle the parachute and man drawing when a player
+    guesses a letter wrong.
+
+    Attributes:
+        _para1(string): parachute line #1
+        _para2(string): parachute line #2
+        _para3(string): parachute line #3
+        _para4(string): parachute line #4
+        _parachute(string): concatenated version of a whole parachute
+        _man1(string): man line #1
+        _man2(string): man line #2
+        _man3(string): man line #3
+        _man4(string): man line #4
+        _man(string): concatenated version of a man
+        _lastman(string): x face of a man
+        _size(integer): size of a parachute 
+    """
     def __init__(self):
+        """Sets the initial value for parachute and man.
+
+        Args:
+            self(Parachute): an instance of Parachute
+        """
         
-        self.para1 = '  ___'
-        self.para2 = " /___\\"
-        self.para3 = " \\   /"
-        self.para4 = "  \\ /"
-        self.parachute = f"{self.para1}\n{self.para2}\n{self.para3}\n{self.para4}"
+        self._para1 = '  ___'
+        self._para2 = " /___\\"
+        self._para3 = " \\   /"
+        self._para4 = "  \\ /"
+        self._parachute = f"{self._para1}\n{self._para2}\n{self._para3}\n{self._para4}"
     
 
-        self.man1 = "   o"
-        self.man2 = "  /|\ "
-        self.man3 = "  / \ "
-        self.man4 = ' '
-        self.man5 = "^^^^^^^"
-        self.man = f"{self.man1}\n{self.man2}\n{self.man3}\n{self.man4}\n{self.man5}"
-        self.lastman = "   x"
+        self._man1 = "   o"
+        self._man2 = "  /|\ "
+        self._man3 = "  / \ "
+        self._man4 = ' '
+        self._man5 = "^^^^^^^"
+        self._man = f"{self._man1}\n{self._man2}\n{self._man3}\n{self._man4}\n{self._man5}"
+        self._lastman = "   x"
 
-        self.parachute_exist = True
-        self.size = 4
+        self._size = 4
         
 
-    def get_size(self):   
-        self.size = self.size - 1   
+    def get_size(self):
+        """Decrement the size of parachute
+
+        Arg: self(Parachute): an instance of Parachute
+        """   
+        self._size = self._size - 1   
 
 
     def set_parachute(self):
-        if self.size == 4:
-            self.parachute = f"{self.para1}\n{self.para2}\n{self.para3}\n{self.para4}"
-        elif self.size == 3:
-            self.parachute = f"{self.para2}\n{self.para3}\n{self.para4}" 
-        elif self.size == 2:
-            self.parachute = f"{self.para3}\n{self.para4}" 
-        elif self.size == 1:
-            self.parachute = f"{self.para4}" 
-        elif self.size == 0:
-            self.parachute = ''
+        """Construct a new Parachute.
 
-        return self.parachute    
+        Args: 
+            self(Parachute): an instance of Parachute
+
+        Returns:
+            a parachute with correct number of lines    
+        """
+        if self._size == 4:
+            self._parachute = f"{self._para1}\n{self._para2}\n{self._para3}\n{self._para4}"
+        elif self._size == 3:
+            self._parachute = f"{self._para2}\n{self._para3}\n{self._para4}" 
+        elif self._size == 2:
+            self._parachute = f"{self._para3}\n{self._para4}" 
+        elif self._size == 1:
+            self._parachute = f"{self._para4}" 
+        elif self._size == 0:
+            self._parachute = ''
+
+        return self._parachute 
+
 
     def set_man(self):
-        if self.size == 0:
-            self.man = f"{self.lastman}\n{self.man2}\n{self.man3}\n{self.man4}\n{self.man5}" 
-        else:
-            self.man = f"{self.man1}\n{self.man2}\n{self.man3}\n{self.man4}\n{self.man5}"
+        """Sets a string for man according to the size.
 
-        return self.man  
+        Args:
+            self(Parachute): an instance of Parachute
+
+        Returns:
+            a man with face 'o' or 'x'    
+        """
+        if self._size == 0:
+            self._man = f"{self._lastman}\n{self._man2}\n{self._man3}\n{self._man4}\n{self._man5}" 
+        else:
+            self._man = f"{self._man1}\n{self._man2}\n{self._man3}\n{self._man4}\n{self._man5}"
+
+        return self._man  
 
 
     def has_parachute(self):
-        if self.parachute != '':
+        """Checks if a parachute is empty.
+
+        Args:
+            self(Parachute): an instance of Parachute
+
+        Returns:
+            True if there are more parachute that could be drawn.    
+        """
+        if self._parachute != '':
             return True       
